b30c42465d9751133a4692e2ad956cadc740cc63
Maffin <maftuna.nova@gmail.com>
2021-12-01 07:33:34 +0500

    js-hw1

--- script.js
+++ script.js
@@ -2,19 +2,38 @@
 
 do {
     var number = +prompt('Введите число') 
-}while(isNaN(number) || number === 0){
+}while(isNaN(number) || number == 0){
    console.log(number);
 }
 
 do{
     var power = +prompt('Введите степень')
-}while(isNaN(power) || power === 0){
+}while(isNaN(power) || power == 0){
    console.log(power);
 } 
 
-var result = (number**power)
 
-alert(result)
+let a = 1
+ for(let i = 0; i < power; i++){
+     a = a * number
+ }
+console.log(a);
 
 
-console.log(result);
+// do {
+//     var num = +prompt('Введите число')
+// }while(isNaN(num) || num == 0)
+
+
+// do {
+//     var power = +prompt('Введите степень')
+// }while(isNaN(power) || power == 0)
+
+
+// let a = 1;
+
+// for(let i = 0; i < power; i++) {
+//     a = a * num
+// }
+
+// console.log(a);
